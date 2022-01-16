019b7525bbbf9ebff032e29eea9bfaa08cb5c8e4
chandrakumarreddy <chandrapenugonda655@gmail.com>
2022-01-16 13:17:51 +0530

    potstop connect to wallets

---
+++ potstop/components/Account.js
@@ -0,0 +1,14 @@
+import EthName from "./EthName";
+
+const Account = function ({ connect }) {
+  // TODO!!!
+  // if already logged in, it should show
+  // the EthName component with the correct address
+  // if not logged in, show a connect button
+  // that when its clicked, will prompt us to login
+  // and store the info on the page
+
+  return <button onClick={connect}>Connect</button>;
+};
+
+export default Account;
